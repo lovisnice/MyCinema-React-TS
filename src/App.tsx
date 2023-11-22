608af7e59764e324215abbcc1505259c31a6e64c
TSShrol <shrolts@gmail.com>
2023-11-22 20:03:43 +0200

    Added Header by AppBar MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Movies from './components/Movies';
+import Home from './components/Home';
+
 
 function App() {
   return (
     <div className="App">
-      <h2> App React TypeScript </h2>
+      {/* <h2> App React TypeScript </h2> */}
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="movies" element={<Movies />} />
+        </Route>
+      </Routes>
+
     </div>
   );
 }
