73bee2f6c3447fedc1c1900f3a930a152b90e46f
User <User@DESKTOP-S7FDFL8>
2023-11-24 18:18:08 +0200

    my

--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -1,12 +1,9 @@
+import MoviesList from "./movies/MovieCard";
 const Movies=()=>{
     return (
         <>
         <h2>Info Movie</h2>
-        <ul>
-            <li>Movie 1</li>
-            <li>Movie 2</li>
-            <li>Movie 3</li>
-        </ul>
+        <MoviesList></MoviesList>
         </>
     )
 }
---
+++ src/components/movies/MoviesList.tsx
@@ -0,0 +1,19 @@
+import MovieCard from "./MovieCard";
+ 
+const MoviesList=()=>{
+    return(
+        <>
+        <h2>Movies List</h2>
+        <div>
+            <MovieCard/>
+            <MovieCard/>
+            <MovieCard/>
+            <MovieCard/>
+            <MovieCard/>
+            <MovieCard/>
+        </div>
+        </>
+       
+    );
+}
+export default MoviesList;
